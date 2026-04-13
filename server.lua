a893d9bb659c8037dc513bf30487716cf9346f6d
RealisticScripts <realisticscripts@gmail.com>
2026-04-12 20:51:37 -0700

    Update version check logic

--- server.lua
+++ server.lua
@@ -1,31 +1,52 @@
 local currentVersion = 'v1.0.0'
+local repoName = 'rs-ignition' -- change per script
 
 local function fetchLatestVersion(callback)
-    PerformHttpRequest('https://api.github.com/repos/RealisticScripts/rs-ignition/releases/latest', function(statusCode, response)
+    local url = ('https://api.github.com/repos/RealisticScripts/%s/releases/latest'):format(repoName)
+
+    local headers = {
+        ['User-Agent'] = ('%s-version-check'):format(repoName),
+        ['Accept'] = 'application/vnd.github+json'
+    }
+
+    PerformHttpRequest(url, function(statusCode, response, responseHeaders)
         if statusCode == 200 then
             local data = json.decode(response)
             if data and data.tag_name then
                 callback(data.tag_name)
             else
-                print('[rs-ignition] Failed to fetch the latest version')
+                print(('[%s] Failed to parse latest release data'):format(repoName))
             end
+        elseif statusCode == 403 then
+            print(('[%s] GitHub API returned 403. Likely rate-limited.'):format(repoName))
+            if response then
+                print(('[%s] Response: %s'):format(repoName, response))
+            end
+        elseif statusCode == 404 then
+            print(('[%s] Release endpoint not found. Check repo name or whether a release exists.'):format(repoName))
         else
-            print(('[rs-ignition] HTTP request failed with status code: %s'):format(statusCode))
+            print(('[%s] HTTP request failed with status code: %s'):format(repoName, statusCode))
+            if response then
+                print(('[%s] Response: %s'):format(repoName, response))
+            end
         end
-    end, 'GET')
+    end, 'GET', '', headers)
 end
 
 local function checkForUpdates()
     fetchLatestVersion(function(latestVersion)
         if currentVersion ~= latestVersion then
-            print('[rs-ignition] A new version of the script is available!')
-            print(('[rs-ignition] Current version: %s'):format(currentVersion))
-            print(('[rs-ignition] Latest version: %s'):format(latestVersion))
-            print('[rs-ignition] Please update the script from: https://github.com/RealisticScripts/rs-ignition')
+            print(('[%s] A new version is available!'):format(repoName))
+            print(('[%s] Current version: %s'):format(repoName, currentVersion))
+            print(('[%s] Latest version: %s'):format(repoName, latestVersion))
+            print(('[%s] Update here: https://github.com/RealisticScripts/%s'):format(repoName, repoName))
         else
-            print('[rs-ignition] Your script is up to date!')
+            print(('[%s] Your script is up to date!'):format(repoName))
         end
     end)
 end
 
-checkForUpdates()
+CreateThread(function()
+    Wait(math.random(5000, 20000)) -- stagger requests to reduce rate-limit hits
+    checkForUpdates()
+end)
